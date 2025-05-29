9c16332cd91f552b12a3804d51c7c2917468dd30
MorettiAndrea <andrea.moretti1704@gmail.com>
2025-05-29 18:21:17 +0200

    pausa quasi finito da ricontrollare

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,58 @@
-// imports
-
 import { useState, useEffect } from "react";
 import axios from "axios";
 const apiUrl = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts";
 
-const formInitialData = { author: "", title: "", body: "", public: false };
+const formInitialData = { author: "", title: "", body: "", public: undefined };
 
 export default function App() {
   const [formDatas, setFormDatas] = useState(formInitialData);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(apiUrl, formDatas)
+      .then((response) => {
+        const { author, title, body } = response.data;
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Errore nel recupero dati:", error);
+      });
+  };
+  const handleInputChange = (e) => {
+    setFormDatas({ ...formDatas, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="container">
       <div className="row">
         <div className="custom-col-1-3">
-          <form action="">
-            <label htmlFor="">
-              <input type="text" />
-            </label>
+          <form action="" onSubmit={handleFormSubmit}>
+            <label htmlFor="autore">Autore</label>
+            <input
+              type="text"
+              value={formDatas.author}
+              name="author"
+              required
+              onChange={handleInputChange}
+            />
+            <label htmlFor="title">Titolo</label>
+            <input
+              type="text"
+              value={formDatas.title}
+              name="title"
+              required
+              onChange={handleInputChange}
+            />
+            <label htmlFor="body">Descrizione</label>
+            <input
+              type="text"
+              value={formDatas.body}
+              name="body"
+              required
+              onChange={handleInputChange}
+            />
+            <button type="submit">CreaPost</button>
           </form>
         </div>
       </div>
