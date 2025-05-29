a0bfae218271e978625e62d29c7931e86907753b
MorettiAndrea <andrea.moretti1704@gmail.com>
2025-05-29 16:53:26 +0200

    creazione struttura

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,26 @@
+// imports
+
 import { useState, useEffect } from "react";
+import axios from "axios";
+const apiUrl = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts";
+
+const formInitialData = { author: "", title: "", body: "", public: false };
+
+export default function App() {
+  const [formDatas, setFormDatas] = useState(formInitialData);
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="custom-col-1-3">
+          <form action="">
+            <label htmlFor="">
+              <input type="text" />
+            </label>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+{
+}
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import * as bootstrap from "bootstrap";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  // <StrictMode>
+  <App />
+  // </StrictMode>
 );
